c50eaaf84c5987b3b0af440d41c65b81daccc218
Philippe Bruhat (BooK) <book@cpan.org>
2013-06-10 23:22:34 +0000

    Import of BOOK/Task-MetaSyntactic-1.028 from CPAN.
    gitpan-cpan-distribution: Task-MetaSyntactic
    gitpan-cpan-version:      1.028
    gitpan-cpan-path:         BOOK/Task-MetaSyntactic-1.028.tar.gz
    gitpan-cpan-author:       BOOK
    gitpan-cpan-maturity:     released

--- lib/Task/MetaSyntactic.pm
+++ lib/Task/MetaSyntactic.pm
@@ -1,9 +1,10 @@
 package Task::MetaSyntactic;
 {
-  $Task::MetaSyntactic::VERSION = '1.027';
+  $Task::MetaSyntactic::VERSION = '1.028';
 }
 
 use strict;
+use warnings;
 
 'whack_eth';
 
@@ -17,7 +18,7 @@ Task::MetaSyntactic - All Acme::MetaSyntactic themes at once
 
 =head1 VERSION
 
-version 1.027
+version 1.028
 
 =head1 SYNOPSIS
 
@@ -56,39 +57,93 @@ L<Acme::MetaSyntactic> themes, in a single command (see the L<SYNOPSIS>).
 
 =head1 CONTENTS
 
-Acme::MetaSyntactic - Themed metasyntactic variables names
+L<Acme::MetaSyntactic> - Themed metasyntactic variables names (BOOK)
 
-Acme::MetaSyntactic::Themes - The original Acme::MetaSyntactic themes
+L<Acme::MetaSyntactic::Themes> - The original Acme::MetaSyntactic themes (BOOK)
 
-Acme::MetaSyntactic::Themes::Abigail - More themes by Abigail (ABIGAIL)
+L<Acme::MetaSyntactic::Themes::Abigail> - More themes by Abigail (ABIGAIL)
 
-Acme::MetaSyntactic::buzzwords - Pointy-haired boss lingo (JQUELIN)
+L<Acme::MetaSyntactic::buzzwords> - Pointy-haired boss lingo (JQUELIN)
 
-Acme::MetaSyntactic::daleks - EXTERMINATE! EXTERMINATE! EXTERMINATE! (SAPER)
+L<Acme::MetaSyntactic::daleks> - EXTERMINATE! EXTERMINATE! EXTERMINATE! (SAPER)
 
-Acme::MetaSyntactic::dune - For he is the Kwisatz Haderach! (MCARTMELL)
+L<Acme::MetaSyntactic::dune> - For he is the Kwisatz Haderach! (MCARTMELL)
 
-Acme::MetaSyntactic::errno - The Errno theme (BOOK)
+L<Acme::MetaSyntactic::errno> - The Errno theme (BOOK)
 
-Acme::MetaSyntactic::legobatman - LEGO Batman theme (BINGOS)
+L<Acme::MetaSyntactic::legobatman> - LEGO Batman theme (BINGOS)
 
-Acme::MetaSyntactic::legoharrypotter - LEGO Harry Potter theme (BINGOS)
+L<Acme::MetaSyntactic::legoharrypotter> - LEGO Harry Potter theme (BINGOS)
 
-Acme::MetaSyntactic::legoindianajones - LEGO Indiana Jones theme (BINGOS)
+L<Acme::MetaSyntactic::legoindianajones> - LEGO Indiana Jones theme (BINGOS)
 
-Acme::MetaSyntactic::legolotr - LEGO Lord of the Rings theme (BINGOS)
+L<Acme::MetaSyntactic::legolotr> - LEGO Lord of the Rings theme (BINGOS)
 
-Acme::MetaSyntactic::legopiratesofthecaribbean - LEGO Pirates of the Caribbean theme (BINGOS)
+L<Acme::MetaSyntactic::legopiratesofthecaribbean> - LEGO Pirates of the Caribbean theme (BINGOS)
 
-Acme::MetaSyntactic::morning_musume - The Mumusu theme (ELBEHO)
+L<Acme::MetaSyntactic::morning_musume> - The Mumusu theme (ELBEHO)
 
-Acme::MetaSyntactic::nethack - The largest time waster in the world of *nix (ELLIOTJS)
+L<Acme::MetaSyntactic::nethack> - The largest time waster in the world of *nix (ELLIOTJS)
 
-Acme::MetaSyntactic::soviet - NATO codenames for Soviet-designed equipment (JFORGET)
+L<Acme::MetaSyntactic::soviet> - NATO codenames for Soviet-designed equipment (JFORGET)
 
-Acme::MetaSyntactic::vim - The vim theme (ELLIOTJS)
+L<Acme::MetaSyntactic::vim> - The vim theme (ELLIOTJS)
 
-Acme::MetaSyntactic::xkcdcommon1949 - xkcd common wordlist for Acme::MetaSyntactic (MARKF)
+L<Acme::MetaSyntactic::xkcdcommon1949> - xkcd common wordlist for Acme::MetaSyntactic (MARKF)
+
+=head1 OTHER METASYNTACTIC MODULES
+
+The following modules (ordered by first release date) are not part of
+this L<Task>, but they also make use L<Acme::MetaSyntactic>:
+
+=over 4
+
+=item L<Bot::MetaSyntactic> (SAPER)
+
+First released on 2005-03-05.
+
+Because the only logical thing to do with L<Acme::MetaSyntactic> was
+obviously an IRC bot. This bot was part of the C<#perlfr> bot menagerie
+for eight years, until it was replaced by the new kid on the block.
+
+=item L<Acme::MetaSyntactic::RefactorCode> (JFENAL)
+
+First released on 2005-03-15.
+
+Based on L<Acme::Floral>, this module refactors source code by replacing
+variable names with L<Acme::MetaSyntactic> items.
+
+=item L<Acme::BooK::Is::My::Bitch> (COG)
+
+First released on 2006-09-02.
+
+This module was written in a hotel room in Birmingham, after the YAPC
+Europe 2006 auction. See also L<http://book.ismybit.ch/>.
+
+=item L<Bot::BasicBot::Pluggable::Module::MetaSyntactic> (BOOK)
+
+First released on 2013-04-13.
+
+Because there's more than one way to do it, and it's easier to write new
+code than patch old code, this bot does what L<Bot::MetaSyntactic> did,
+with better support for themes with sub-categories, and the ability to
+grep for items.
+
+The bot hangs out on C<#perlfr> and C<#p5p> on L<irc://irc.perl.org/>.
+
+=item L<Acme::PPIx::MetaSyntactic> (TOBYINK)
+
+First released on 2013-05-15.
+
+As we've seen, there's more than one way to be metasyntactic.
+
+This module renames functions and variables in a L<PPI::Document> using
+L<Acme::MetaSyntactic> themes.
+
+This is the application that L<Acme::MetaSyntactic> was created for.
+It only took eight years.
+
+=back
 
 =head1 BUGS
 
